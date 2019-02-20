3f8541c116dc3ba55cd0c4688b512ce100d6db60
Allan Orellana <allan_ov1995@hotmail.com>
2019-02-20 09:30:08 -0500

    Funciones Genericas para el api parte comercial
    por el omento contiene los metodos de post,get,delete

---
+++ src/api/goApi.js
@@ -0,0 +1,80 @@
+import axios from 'axios'
+var apiUrlBase = 'localhost:8005'
+
+export default {
+  getAll(url, data) {
+    axios.get(apiUrlBase + url)
+    .then(response => {
+      if (response.status !== 200) {
+        data.error = response.statusText
+        return data
+      }
+      var data = response.data
+      data.dataGet = Object.values(data)
+    })
+    .catch((err) => {
+    // Request failed.
+      data.error = err
+      console.log('FAIL', err)
+    })
+    return data
+  },
+  // para pedir campos en especifico, se pasa un objeto con los parametros del get y sus valores
+  getCustom(objectFields, url, data) {
+    axios.get(
+      apiUrlBase + url,
+      {params:
+        objectFields
+      }
+    )
+    .then(response => {
+      if (response.status !== 200) {
+        data.error = response.statusText
+        return data
+      }
+      var data = response.data
+      data.dataGet = Object.values(data)
+    })
+    .catch(error => {
+      // Request failed.
+      data.error = error
+    })
+    return data
+  },
+    // se realiza un post dependiedo lo que almacene dataPostDel
+    // se asume que exista un binding entre los campos del form con los de dataPostDel
+  post(url, data) {
+    axios.post(
+    apiUrlBase + url,
+    data.dataPostDel // esto debe cambiar acuerdo a como se use el form
+    )
+    .then(response => {
+      if (response.status !== 200) {
+        data.error = response.statusText
+        return data
+      }
+    })
+    .catch(error => {
+      // Request failed.
+      data.error = error
+    })
+    return data
+  },
+  detele(url, data) {
+    axios.delete(
+    apiUrlBase + url,
+    data.dataPostDel
+    )
+    .then(response => {
+      if (response.status !== 200) {
+        data.error = response.statusText
+        return data
+      }
+    })
+    .catch(error => {
+      // Request failed.
+      data.error = error
+    })
+    return data
+  }
+}
