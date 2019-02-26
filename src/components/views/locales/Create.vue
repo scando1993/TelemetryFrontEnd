083c167e68476f317df663dd008d227f56c4e458
Jocelyn Miranda <jocammir@espol.edu.ec>
2019-02-26 16:32:28 -0500

    Locales
    65%

--- src/components/views/locales/Create.vue
+++ src/components/views/locales/Create.vue
@@ -1,101 +1,84 @@
 <template>
-  <!-- Main content -->
   <section class="content">
-    <div class="col-xs-12">
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title"></h3>
-          <div class="box-body">
-            <div class="col-sm-12 col-xs-12">
-              <iframe name="hiddenFrame" class="hide"></iframe>
-              <form v-on:submit='save' method="POST" target="hiddenFrame" class="form-horizontal" id="profile-form">
-                <div class="form-group">
-                  <label class="col-sm-4 control-label">No.Loc</label>
-                  <div class="col-sm-9 controls">
-                    <input type="number" class="form-control" v-bind:placeholder="dato.numLoc" v-model="dataPostDel.numLoc" name="name" id="noLoc" maxlength="50" value="">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-4 control-label">Nombre</label>
-                  <div class="col-sm-9 controls">
-                    <input type="text" class="form-control" v-bind:placeholder="dato.name" v-model="dataPostDel.name" name="name" id="name" maxlength="50" value="">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-4 control-label">Lugar</label>
-                  <div class="col-sm-9 controls">
-                    <input type="text" class="form-control" v-bind:placeholder="dato.place" v-model="dataPostDel.place" name="name" id="place" maxlength="50" value="">
-                  </div>
-                </div>                
-                <div class="form-group">
-                  <label class="col-sm-5  control-label">Ubicaciones</label>
-                  <div class="col-sm-9  controls">
-                    <select v-model="selectedLocal">
-                      <option disabled value="">Por favor seleccionar uno</option>
-                      <option v-for="datoL in ubications.dataGet ">{{ datoL.zone }}</option>
-                    </select>
-                  </div>
-                </div>
-              </form>
+    <div class="span12">
+      <div class="row center-block">
+        <div class="col-md-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Añadir un nuevo Local</h3>
             </div>
-            <hr class="visible-xs-block">
-            <div class="col-sm-12 col-xs-12">
-              <p class="text-center">
-                <strong>Seleccione la ubicación en el mapa: </strong>
-                <div class="text-center">
-                  <label>
-                    <gmap-autocomplete
-                      @place_changed="setPlace">
-                    </gmap-autocomplete>
-                    <button @click="addMarker">Añadir marcador</button>
-                  </label>
-                  <br/>
-                </div>
-                <br/>
-              </p>
-              <!--<canvas id="languagePie"></canvas>-->
-              <google-map /><br />
-            </div>            
-              <div class="col-sm-6 col-xs-15">
-                <!-- Submit and cancel -->
-                <div class="form-group">
-                  <div class="text-center">
-                  <div class="col-sm-12 col-sm-offset-4 col-lg-12 col-lg-offset-6">
-                    <router-link class="pageLink" to="/locals">
-                      <button type="submit" class="btn btn-primary" v-on:submit='save'><i class="fa fa-ok"></i> Guardar </button>
-                      <a href="/locals" type="button" class="btn">Cancelar</a>
-                    </router-link>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="box-content">
+                <iframe name="hiddenFrame" class="hide"></iframe>
+                <form action="/create" method="POST" target="hiddenFrame" class="form-horizontal" id="profile-form">
+                  <div class="form-group">
+                    <label class="col-sm-3 col-lg-2 control-label">No.Local</label>
+                    <div class="col-sm-9 col-lg-10 controls">
+                      <input type="number" class="form-control" v-model="dataPostDel.numLoc" name="name" maxlength="50" value="">
+                    </div>
                   </div>
-                </div>
-                </div>
-                <!--End Submit and cancel-->
+                  <div class="form-group">
+                    <label class="col-sm-3 col-lg-2 control-label">Nombre</label>
+                    <div class="col-sm-9 col-lg-10 controls">
+                      <input type="text" class="form-control" v-model="dataPostDel.name" name="ruta" maxlength="50" value="">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-3 col-lg-2 control-label">Lugar</label>
+                    <div class="col-sm-9 col-lg-10 controls">
+                      <input type="text" class="form-control" v-model="dataPostDel.place" name="ruta" maxlength="50" value="">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-3 col-lg-2 control-label">Longitud</label>
+                    <div class="col-sm-9 col-lg-10 controls">
+                      <input type="number" class="form-control" v-model="dataPostDel.length" name="name" maxlength="50" value="">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-3 col-lg-2 control-label">Latitud</label>
+                    <div class="col-sm-9 col-lg-10 controls">
+                      <input type="number" class="form-control" v-model="dataPostDel.latitude" name="ruta" maxlength="50" value="">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-3 col-lg-2 control-label">Ubicaciones</label>
+                    <div class="col-sm-9 col-lg-10 controls">
+                      <select v-model="selectedLocal">
+                        <option disabled value="">Por favor seleccionar uno</option>
+                        <option v-for="datoL in dataGet ">{{ datoL.zone }}</option>
+                      </select>
+                    </div>
+                  </div>
+                  <!-- Submit and cancel -->
+                  <div class="form-group">
+                    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2">
+                      <router-link class="pageLink" to="/locals">
+                        <button type="submit" class="btn btn-primary" v-on:click="save"><i class="fa fa-ok"></i> Guardar</button>
+                        <a href="/format" type="button" class="btn">Cancelar</a>
+                      </router-link>
+                    </div>
+                  </div>
+                  <!--End Submit and cancel-->
+
+                </form>
               </div>
+              <!-- /.box-body -->
             </div>
+            <!--End Box-->
           </div>
+        </div>
+
       </div>
     </div>
+    <!--</div>-->
+    <!--End content-->
   </section>
-  <!-- /.content -->
 </template>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script>
   import api from '@/api/goApi.js'
-  import GoogleMap from './../../Geocalization/GoogleMap.vue'
   export default {
-    components: {
-      GoogleMap
-    },
-    computed: {
-      coPilotNumbers() {
-        return this.generateRandomNumbers(12, 1000000, 10000)
-      },
-      personalNumbers() {
-        return this.generateRandomNumbers(12, 1000000, 10000)
-      },
-      isMobile() {
-        return (window.innerWidth <= 800 && window.innerHeight <= 600)
-      }
-    },
     methods: {
       updateData(newData) {
         this.error = newData.error
@@ -124,15 +107,11 @@
         selectedLocal: '',
         dataGet: [],
         dataPostDel: { // este es basicamente un JSON
+          numLoc: 0,
           name: '',
-          ruta: ''
-        },
-        generateRandomNumbers(numbers, max, min) {
-          var a = []
-          for (var i = 0; i < numbers; i++) {
-            a.push(Math.floor(Math.random() * (max - min + 1)) + max)
-          }
-          return a
+          length: 0,
+          latitude: 0,
+          place: ''
         }
       }
     },
@@ -142,19 +121,3 @@
     }
   }
 </script>
-<style>
-
-  .info-box {
-    cursor: pointer;
-  }
-
-  .info-box-content {
-    text-align: center;
-    vertical-align: middle;
-    display: inherit;
-  }
-
-  .fullCanvas {
-    width: 100%;
-  }
-</style>
