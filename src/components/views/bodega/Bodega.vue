a92d55c45b4c295b38b8129e391ac065365bc9f9
Allan <allan_ov1995@hotmail.com>
2019-02-28 01:30:03 -0500

    Arregrado Editar

--- src/components/views/bodega/Bodega.vue
+++ src/components/views/bodega/Bodega.vue
@@ -71,7 +71,7 @@
                                   <div class="modal-content">
                                     <!--modal header-->
                                     <div class="modal-header">
-                                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                      <button type="button" class="close" data-dismiss="modal" @click="editar(index)" aria-hidden="true">&times;</button>
                                       <h4 class="modal-title">Editar</h4>
                                     </div>
                                     <!--end modal-header-->
--- src/components/views/boxcar/Boxcar.vue
+++ src/components/views/boxcar/Boxcar.vue
@@ -74,7 +74,7 @@
                                 <div class="modal-content">
                                   <!--modal header-->
                                   <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                    <button type="button" class="close" data-dismiss="modal" @click="editOne(index)" aria-hidden="true">&times;</button>
                                     <h4 class="modal-title">Editar</h4>
                                   </div>
                                   <!--end modal-header-->
